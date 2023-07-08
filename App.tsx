d3d49e28de4199d5e3c650202f015185f7188a02
Sasha <alexanderakorukova@Sasha.local>
2023-07-08 17:00:35 +0200

    nothing

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import Monster from './Monster';
 import Favourites from './Favourites';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5Brands from 'react-native-vector-icons/FontAwesome5'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 const Tab = createBottomTabNavigator();
@@ -10,8 +12,26 @@ const Tab = createBottomTabNavigator();
 function Tabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Favourites" component={Favourites} />
-      <Tab.Screen name="Monster" component={Monster} />
+      <Tab.Screen 
+        name="Favourites" 
+        component={Favourites} 
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({color, size}) => (
+            <Icon name='heart' color='#00DDCC' size={20}/>
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="Monster" 
+        component={Monster} 
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: () => (
+            <FontAwesome5Brands name='octopus-deploy' color='#00DDCC' size={20}/>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
